1eef4d54719884b3a3edab940f54789c2df88ed9
Kanishe <a.kan4988@gmail.com>
2019-06-04 17:25:51 +0300

    init commit;

---
+++ src/main/java/Scops.java
@@ -0,0 +1,19 @@
+public class Scops {
+  public static void main(String[] args) {
+
+    int n, d, q;
+    n = 10;
+    d = 2;
+    q = n/d;
+    if (d != 0 && (n % d) == 0)
+      System.out.println( d+ " является делением" + n + " "+ q);
+
+    d =0;
+
+      System.out.println( d+ " является делением" + n);
+
+
+    if (d != 0 & (n % d) == 0)
+      System.out.println( d+ " является делением" + n);
+  }
+}
