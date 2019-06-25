397c8285eaebc01551d77ce09f25cf55b99ef849
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-06-25 23:35:35 +0300

    new lesson

--- src/main/java/Parag4/RetMeth2.java
+++ src/main/java/Parag4/RetMeth2.java
@@ -5,24 +5,28 @@ class RetMeth2 {
     Vehicle2 miniven = new Vehicle2();
     Vehicle2 sportcar = new Vehicle2();
 
-    int range1, range2;
+    int rangeminiven, rangesportcar;
 
-    //Присваивание значений полям объекта sportcar object.member
+
     miniven.passengers = 7;
     miniven.fuelcap = 16;
     miniven.mpg = 21;
 
-    //Присваивание значений полям объекта sportcar object.member
-    sportcar.fuelcap=14;
-    sportcar.passengers=2;
+
+    sportcar.passengers = 2;
+    sportcar.fuelcap = 14;
     sportcar.mpg=12;
 
 
-    range1=miniven.range();
-    range2=sportcar.range();
 
-    System.out.println("Mини-фypгoн"+ miniven.passengers + " на расстояние "+ range1 + " миль.");
-    System.out.println("Спорткар"+ sportcar.passengers + " на расстояние " +range2 + " миль.");
+    rangeminiven=miniven.range();
+    rangesportcar=sportcar.range();
+    
+
+
+    System.out.println("Фургон" + miniven.passengers + "проедет" + rangeminiven);
+    System.out.println(" Спорткар" + sportcar.passengers + "проедет" + rangesportcar);
+
 
 
   }
