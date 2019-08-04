a1621ac65602ad1c1ff11b62cb95cb7839ad1db1
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-08-04 23:24:07 +0300

    agan repeat constructor 04/08/2019

--- src/main/java/Konstructor/konstructor04082019/Homo.java
+++ src/main/java/Konstructor/konstructor04082019/Homo.java
@@ -1,4 +1,48 @@
 package Konstructor.konstructor04082019;
 
 public class Homo {
+  public static void main(String[] args) {
+    Person person1=new Person();
+    System.out.println(person1.getName()+"\n"+person1.getAge());
+
+  }
+}
+
+class Person{
+
+  private int age;
+  private String name;
+
+  //constructors
+  public Person(){
+    this.name= "default name";
+    this.age= 0;
+    System.out.println("hi, from first constructor!");
+  }
+  public Person(String name){
+    this.name=name;
+    System.out.println("hi, from second constructor!!");
+  }
+  public Person(String name, int age){
+    this.name=name;
+    this.age=age;
+    System.out.println("hi, from third constructor!!");
+  }
+
+  // getters and setters
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+public void setName(String name) {
+    this.name = name;
+}
 }
