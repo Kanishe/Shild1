7883f5802a15c7ec57baa229209260b55ed0a6dc
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-08-14 23:57:40 +0300

    FIX

--- src/main/java/exeption/Main.java
+++ src/main/java/exeption/Main.java
@@ -4,7 +4,6 @@ package exeption;
 public class Main {
   public static void main(String[] args) {
     ErrorExapleNullPoiter errorExapleNullPoiter =new ErrorExapleNullPoiter();
-    Throwable
     try {
       errorExapleNullPoiter.show();
       System.err.println(1/0);
