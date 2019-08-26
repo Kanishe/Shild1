bcbfe505833e7ff750abee30ac08cfe759c177b3
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-08-26 23:55:33 +0300

    add new ARRAY LIST

--- src/main/java/collection/lesson1/MassivArry.java
+++ src/main/java/collection/lesson1/MassivArry.java
@@ -1,4 +1,39 @@
 package collection.lesson1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MassivArry {
+  public static void main(String[] args) {
+    //ArrayList<Integer> arrayList=new ArrayList<Integer>(); но учитываю конвенцию павильно будет ссылать на интерфейс так:
+    List<Integer> arrayList=new ArrayList<Integer>();
+    for (int i=0; i<100; i++){
+      arrayList.add(i);
+
+    }
+    System.out.println(arrayList);
+
+    System.out.println(arrayList.get(0));
+    System.out.println(arrayList.get(99));
+
+
+    System.out.println(arrayList.size());
+//3 способа пройти по элментам массива
+    //№1
+    for (int i=0; i<arrayList.size();i++){
+      System.out.println(arrayList.get(i));
+    }
+    //№2
+
+    for (Integer x:arrayList){
+      System.out.println(x);
+    }
+    //№3 (лямбда функции)
+
+
+
+
+
+  }
+
 }
---
+++ src/main/java/collection/lesson1/arraylist/Test.java
@@ -0,0 +1,10 @@
+package collection.lesson1.arraylist;
+
+import java.util.ArrayList;
+
+public class Test{
+  public static void main(String[] args) {
+    ArrayList <Integer> arrayList=new ArrayList(1000);
+  }
+
+}
