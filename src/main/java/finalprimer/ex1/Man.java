6aa745f965f24a3bab2af0368675b185867681e1
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-10-15 00:04:21 +0300

    repead keyword FINAL

--- src/main/java/finalprimer/ex1/Man.java
+++ src/main/java/finalprimer/ex1/Man.java
@@ -1,4 +1,19 @@
 package finalprimer.ex1;
-
+//todo final https://www.youtube.com/watch?v=em6TpQd1v1o
 public class Man {
-}
+  public static void main(String[] args) {
+    final int age=3;
+  }
+
+  class One{
+    final void method(){
+      System.out.println("one");
+    }
+  }
+  class Two extends One{
+   //Если откоментить то получим ошибку так как метод в предыдущем шаге обозначен как ФИНАЛЬНЫЙ
+    // void method(){
+     // System.out.println("two");
+    }
+  }
+
