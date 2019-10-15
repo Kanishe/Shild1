76291085b7e77a5db28038c862d21737c79113f1
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-10-15 23:52:52 +0300

    повторил функции

---
+++ src/main/java/javarash/level2/MethodCall.java
@@ -0,0 +1,12 @@
+package javarash.level2;
+public class MethodCall {
+  public static void main(String[] args) {
+    print4("I like to move it, move it.");
+  }
+  public static void print4 (String s) {
+    System.out.println(s);
+    System.out.println(s);
+    System.out.println(s);
+    System.out.println(s);
+  }
+}
